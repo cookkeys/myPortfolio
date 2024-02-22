fb44d8bf91d4e8b4dd0622b3b8ba1896f3b17dfb
cookkeys <snitin.imk@gmail.com>
2024-02-23 03:28:28 +0530

    first code

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import { FaAndroid, FaBriefcase, FaCog, FaGem, FaInstagram, FaNode, FaNodeJs, Fa
 export default function Home() {
   return (
     <div>
+
+
     <div className={styles.outer_div}>
       <div className={styles.inner_div_1}>
         <div>
@@ -19,6 +21,8 @@ export default function Home() {
           <Image src={img1} className={styles.img}></Image>
       </div>
     </div>
+
+    <p className={styles.getInTouch}>My Skills</p>
     <div className={styles.skills}>
       <div  className={styles.skill1}>
         <FaReact className={styles.skill_Icon}/>
@@ -34,6 +38,27 @@ export default function Home() {
       <FaAndroid className={styles.skill_Icon}/>
       <h3>Android</h3>
       <p>Google Career Certificates offer online training that can put you on the fast track to in-demand jobs — no experience necessary.</p>
+      </div>
+    </div>
+
+<p className={styles.getInTouch}>Get in Touch</p>
+
+    <div className={styles.contacts}>
+      <div> 
+        <p>Name</p>
+        <p>Nitin Singh</p>
+      </div>
+      <div>
+        <p>Phone Number</p>
+        <p>1231231231</p>
+      </div>
+      <div>
+        <p>Email</p>
+        <p>xyz@gmail.com</p>
+      </div>
+      <div>
+        <p>LinkedIn</p>
+        <p>xyz</p>
       </div>
     </div>
     </div>
